9a54ff7aadcb5596f034d983796290e9c4e83564
JuanLopera0726 <juanmanuel07266@gmail.com>
2023-05-16 20:48:27 -0500

    Creacion del controlador y las rutas para el profesor

--- src/app/controllers/TeacherController.ts
+++ src/app/controllers/TeacherController.ts
@@ -1,1 +1,33 @@
- 
+import { Teacher } from './../../domain/entities/Teacher';
+import { ITeacherRepository } from "domain/interfaces/ITeacherRepository";
+import { Request, Response } from "express";
+
+export class TeacherController {
+
+    constructor(private _teacherRepository: ITeacherRepository) {
+    }
+
+    public async create( request: Request, response: Response ): Promise<Response> {
+         
+        try {
+
+            const { name, description, email, birthDate } = request.body;
+            const teacher: Teacher = new Teacher( name, description, email, new Date(birthDate) );
+            const createdTeacher = await this._teacherRepository.create( teacher );
+            return response.status(201).json( createdTeacher ); 
+
+        // Jamás puede mostrar en una respuesta cuál es el error (json(error))
+        // Por medio del json se comunica con el cliente
+        } catch (error) {
+            
+            console.log(error);
+            return response.status(500).json({
+                errorMessage: 'Houston tenemos un problema!',
+                code: 9999
+            }); 
+
+        } 
+
+    }
+    
+}
