5d3552325e1ea3f980bff7d71f94c370d285ebc1
JuanLopera0726 <juanmanuel07266@gmail.com>
2023-05-17 21:22:35 -0500

    se crea los middlewares para la api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import { TeacherRoutes } from './app/routes/TeacherRoutes';
 import express from 'express';
 import config from './config';
 
+import cors from './app/middlewares/cors';
+import morgan from './app/middlewares/logger-http'
+import swaggerRouter from './app/middlewares/swagger/swagger';
+
 export class Server {
 
     private _app: express.Application;
@@ -18,8 +22,11 @@ export class Server {
     }
 
     private middlewares(): void {
-        this._app.use( express.json() );
-        this._app.use( express.urlencoded({ extended: false }) );
+        this._app.use(express.json());
+        this._app.use(express.urlencoded({ extended: false }));
+        this._app.use(cors);
+        this._app.use(morgan)
+        this._app.use('/api-docs', swaggerRouter);
     }
 
     private routes(): void {
